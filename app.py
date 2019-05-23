02e54e1dd939a429f7b5e8d1160a93f7043ec847
zhong <zhonglikui@futurebots.cn>
2019-05-23 16:50:02 +0800

    添加自动更新代码

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+import hmac
+
+from flask import Flask, request, jsonify
+from git import Repo
 
 app = Flask(__name__)
 
@@ -8,10 +11,22 @@ def index():
     return app.send_static_file("home.html")
 
 
-@app.route("/webscan_360_cn.html")
-def webscan_360_cn():
-    return app.send_static_file("webscan_360_cn.html")
+@app.route("/github", methods=["POST"])
+def github():
+    signature = request.headers.get('X-Hub-Signature')
+    sha, signature = signature.split('=')
+    secret = str.encode(app.config.get('GITHUB_SECRET'))
+    hashhex = hmac.new(secret, request.data, digestmod='sha1').hexdigest()
+    if hmac.compare_digest(hashhex, signature):
+        repo = Repo(app.config.get('REPO_PATH'))
+        origin = repo.remotes.origin
+        origin.pull('--rebase')
+        commit = request.json['after'][0:6]
+        print('Repository updated with commit {}'.format(commit))
+    return jsonify({}), 200
 
 
 if __name__ == "__main__":
+    app.config['GITHUB_SECRET'] = "web"
+    app.config['REPO_PATH'] = "/usr/share/nginx/web"
     app.run(debug=False)
