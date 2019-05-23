af401314049122727d780eeede9bf0c45738bf3a
zhong <zhonglikui@futurebots.cn>
2019-05-23 11:51:09 +0800

    提交安全验证文件

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def index():
     return app.send_static_file("home.html")
 
 
+@app.route("/webscan_360_cn.html")
+def webscan_360_cn():
+    return app.send_static_file("webscan_360_cn.html")
+
+
 if __name__ == "__main__":
     app.run(debug=False)
