dd6325b26e59acb3d10735d77b4da56eb490afd8
zhong <zhonglikui@futurebots.cn>
2019-05-23 17:04:50 +0800

    更新代码

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-import hmac
-
-from flask import Flask, request, jsonify
-from git import Repo
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -11,19 +8,20 @@ def index():
     return app.send_static_file("home.html")
 
 
-@app.route("/github", methods=["POST"])
+@app.route("/github", methods=["POST", "GET"])
 def github():
-    signature = request.headers.get('X-Hub-Signature')
-    sha, signature = signature.split('=')
-    secret = str.encode(app.config.get('GITHUB_SECRET'))
-    hashhex = hmac.new(secret, request.data, digestmod='sha1').hexdigest()
-    if hmac.compare_digest(hashhex, signature):
-        repo = Repo(app.config.get('REPO_PATH'))
-        origin = repo.remotes.origin
-        origin.pull('--rebase')
-        commit = request.json['after'][0:6]
-        print('Repository updated with commit {}'.format(commit))
-    return jsonify({}), 200
+    return app.send_static_file("home.html")
+    # signature = request.headers.get('X-Hub-Signature')
+    # sha, signature = signature.split('=')
+    # secret = str.encode(app.config.get('GITHUB_SECRET'))
+    # hashhex = hmac.new(secret, request.data, digestmod='sha1').hexdigest()
+    # if hmac.compare_digest(hashhex, signature):
+    #     repo = Repo(app.config.get('REPO_PATH'))
+    #     origin = repo.remotes.origin
+    #     origin.pull('--rebase')
+    #     commit = request.json['after'][0:6]
+    #     print('Repository updated with commit {}'.format(commit))
+    # return jsonify({}), 200
 
 
 if __name__ == "__main__":
